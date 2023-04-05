26f9e11d0a288ab1d900df203894e8a5d469abb9
nautilo <8lapse@gmail.com>
2023-04-05 02:44:07 -0400

    drag rotate

--- js/tazon.js
+++ js/tazon.js
@@ -1,13 +1,21 @@
-document.addEventListener("mousedown",activarRotacion);
-document.addEventListener("mouseup",desactivarRotacion);
-function activarRotacion(event){
-    document.addEventListener("mousemove",rotar)
-}
-function desactivarRotacion(event){
-    document.removeEventListener("mousemove",rotar)
-}
-function rotar(event) {
-    const x = -event.pageX / 10;
-    document.getElementById("tazon").style.transform = `rotateY(${-x}deg)`;
+let isDragging = false;
+
+document.addEventListener("mousedown", startDrag);
+document.addEventListener("mouseup", endDrag);
+
+function startDrag() {
+  isDragging = true;
+  document.addEventListener("mousemove", rotar);
 }
 
+function endDrag() {
+  isDragging = false;
+  document.removeEventListener("mousemove", rotar);
+}
+
+function rotar(event) {
+  if (isDragging) {
+    const x = event.pageX / 10;
+    document.getElementById("tazon").style.transform = `rotateY(${x}deg)`;
+  }
+}
