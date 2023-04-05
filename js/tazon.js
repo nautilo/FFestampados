5c9c6b81594490a7cf8e7b0175ef48ed59f53bd8
nautilo <8lapse@gmail.com>
2023-04-05 10:52:09 -0400

    Rotate in touchscreen

--- js/tazon.js
+++ js/tazon.js
@@ -1,26 +1,27 @@
 let isDragging = false;
 
 document.addEventListener("mousedown", startDrag);
-document.addEventListener("mouseup", endDrag);
-document.addEventListener("ontouchstart", startDrag);
-document.addEventListener("ontouchend", endDrag);
+document.addEventListener("touchstart", startDrag);
 
+document.addEventListener("mouseup", endDrag);
+document.addEventListener("touchend", endDrag);
 
 function startDrag() {
   isDragging = true;
   document.addEventListener("mousemove", rotar);
-  document.addEventListener("ontouchmove", rotar);
+  document.addEventListener("touchmove", rotar);
 }
 
 function endDrag() {
   isDragging = false;
   document.removeEventListener("mousemove", rotar);
-  document.addEventListener("ontouchmove", rotar);
+  document.removeEventListener("touchmove", rotar);
 }
 
 function rotar(event) {
+  event.preventDefault();
   if (isDragging) {
-    const x = event.pageX / 10;
+    const x = (event.pageX || event.touches[0].pageX) / 10;
     document.getElementById("tazon").style.transform = `rotateY(${x}deg)`;
   }
 }
